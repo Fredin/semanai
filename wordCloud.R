310f0b35218c74c30b006d9f548b89dd87667b01
Esteban Fredin <esteban.fredin@gmail.com>
2016-09-27 11:59:27 -0500

    Fixed worldCloud.R, unicode_escape was deleted as it's no longer needed. Rearranged stuff a bit.

--- wordCloud.R
+++ wordCloud.R
@@ -8,32 +8,28 @@ library(wordcloud)
 library(RColorBrewer)
 
 # Primero hay que logearse a la API de twitter. Por motivos de seguridad omito
-# esa parta.
+# esa parte.
 
 # get the twits
-mach_tweets <- searchTwitter("@FelipeCantuR", n = 1000, lang="es")
+mySearch <- searchTwitter("@realDonaldTrump", n = 100)
 
-mach_text <- sapply(mach_tweets, function(x) x$getText())
+# Get rid of non-utf-8 characters
+rawTweets <- twListToDF(mySearch)$text
+convTweets <- iconv(rawTweets, to = "utf-8")
+tweets <- (convTweets[!is.na(convTweets)])
 
 # create a corpus
-mach_corpus <- Corpus(VectorSource(mach_text),
-                      readerControl = list(language = "spanish"))
-
-# escape unicode. Remember to source the unicode_escape file first.
-mach_corpus <- tm_map(mach_corpus,
-                     content_transformer(function(x) unicode_escape(as.character(x))))
-
+corpus <- Corpus(VectorSource(tweets),
+                      readerControl = list(language = "english"))
 
 # remove URLs
-mach_corpus <- tm_map(mach_corpus,
+corpus <- tm_map(corpus,
                       content_transformer(function(x) gsub("http.*",".",x,ignore.case=TRUE)))
 
 # create document term matrix applying some transformations
-tdm = TermDocumentMatrix(mach_corpus,
-                         control = list(language = "spanish", removePunctuation = TRUE,
-                                        stopwords = c(stopwords("spanish"),
-                                                      "felipe","cantu",
-                                                      "felipecantur"),
+tdm <- TermDocumentMatrix(corpus,
+                         control = list(language = "english", removePunctuation = TRUE,
+                                        stopwords = c(stopwords("english")),
                                         removeNumbers = TRUE, tolower = TRUE))
 # define tdm as matrix
 m = as.matrix(tdm)
@@ -43,7 +39,7 @@ word_freqs = sort(rowSums(m), decreasing=TRUE)
 dm = data.frame(word=names(word_freqs), freq=word_freqs)
 
 # plot wordcloud
-wordcloud(dm$word, dm$freq, random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+wordcloud(dm$word[1:100], dm$freq[1:100], random.order=FALSE, colors=brewer.pal(8, "Dark2"))
 
 # save the image in png format
 png("felipeCloud.png", width=12, height=8, units="in", res=300)
