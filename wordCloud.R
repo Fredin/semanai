6aece14904ec91ba995f71ce8db09744ed14f78b
Esteban Fredin <esteban.fredin@gmail.com>
2016-09-28 13:02:38 -0500

    Resuelto el problema con los emojis.

--- wordCloud.R
+++ wordCloud.R
@@ -1,4 +1,4 @@
-### Esto es >95% código de Gastón Sanchez. El código original y mucho más lo
+### Esto es en gran parte código de Gastón Sanchez. El código original y mucho más lo
 ### pueden encontrar en https://sites.google.com/site/miningtwitter/ Un súper
 ### sitio si apenas estan empezando a minar y visualizar texto con R.
 
@@ -7,31 +7,34 @@ library(tm)
 library(wordcloud)
 library(RColorBrewer)
 
-# Primero hay que logearse a la API de twitter. Por motivos de seguridad omito
-# esa parte.
+# Primero hay que logearse a la API de twitter. Ver twitter_Auth.R
 
 # get the twits
-mySearch <- searchTwitter("paz", n = 500)
-
-
-# Get rid of non-utf-8 characters
-rawTweets <- twListToDF(mySearch)$text
-convTweets <- iconv(rawTweets, to = "utf-8")
-tweets <- (convTweets[!is.na(convTweets)])
+mySearch <- searchTwitter("#semanai", n = 500, since = "2016-09-26")
+tweets <- twListToDF(mySearch)$text
 
 # create a corpus
 corpus <- Corpus(VectorSource(tweets),
                       readerControl = list(language = "spanish"))
 
-# remove URLs
-corpus <- tm_map(corpus,content_transformer(function(x) gsub("http\\S*","", x,ignore.case=TRUE)))
-corpus <- tm_map(corpus,content_transformer(function(x) gsub("@\\S*","", x ,ignore.case=TRUE)))
+# remove non-utf-8 characters, URLs, usernames and hashtags
+corpus <- tm_map(corpus,content_transformer(
+  function(x) iconv(x,"utf-8","latin1", sub = "")))
+corpus <- tm_map(corpus,content_transformer(
+  function(x) gsub("http\\S*","", x,ignore.case=TRUE)))
+corpus <- tm_map(corpus,content_transformer(
+  function(x) gsub("@\\S*","", x,ignore.case=TRUE)))
+corpus <- tm_map(corpus,content_transformer(
+  function(x) gsub("#\\S*","", x ,ignore.case=TRUE)))
 
 # create document term matrix applying some transformations
 tdm <- TermDocumentMatrix(corpus,
-                         control = list(language = "spanish", removePunctuation = TRUE,
-                                        stopwords = stopwords("spanish"),
-                                        removeNumbers = TRUE, tolower = TRUE))
+                         control = list(language = "spanish",
+                                        removePunctuation = TRUE,
+                                        stopwords = c(stopwords("spanish"),
+                                                      "semanai"),
+                                        removeNumbers = TRUE,
+                                        tolower = TRUE))
 # define tdm as matrix
 m = as.matrix(tdm)
 # get word counts in decreasing order
@@ -39,10 +42,12 @@ word_freqs = sort(rowSums(m), decreasing=TRUE)
 # create a data frame with words and their frequencies
 dm = data.frame(word=names(word_freqs), freq=word_freqs)
 
-# plot wordcloud
-wordcloud(dm$word[2:500], dm$freq[2:300], random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+# plot wordcloud and save in png format
 
-# save the image in png format
 png("wordCloud.png", width=12, height=8, units="in", res=300)
-wordcloud(dm$word, dm$freq, random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+
+wordcloud(dm$word, dm$freq,
+          min.freq = 5, max.words = 200,
+          random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+
 dev.off()
